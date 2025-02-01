f4650f05c21dcce586f9c8036ba37a8e9fe87a9a
mdierich <martin.dierich@gmail.com>
2025-02-01 10:42:26 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,4 +6,5 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.setLedColors(0xffff00, 0xffff00, 0xffff00)
     basic.showString("Post vorhanden")
+    music.playMelody("G F G A C5 F E D ", 120)
 })
