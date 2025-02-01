c79ddeaf01590d37ac82b01b9dee5bf9a2aaf8bb
mdierich <martin.dierich@gmail.com>
2025-02-01 10:37:10 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("C5 A C5 A C5 A C5 A ", 120)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.setLedColors(0xffff00, 0xffff00, 0xffff00)
+    basic.showString("Post vorhanden")
+})
